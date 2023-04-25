ea1e8b35dc18b449d4befc67ea394feadf82ef1c
jeppjeppjepp0 <jeddmj1@gmail.com>
2023-04-24 22:34:46 -0600

    added jest & finished some prompting

--- index.js
+++ index.js
@@ -5,23 +5,47 @@ function init() {
     .prompt([
         {
             type: 'input',
-            message: 'What is the title of your project?',
-            name: 'title',
+            message: 'Enter up to 3 characters of text',
+            name: 'text',
+            validate: function (input) { 
+                return input.length > 0 && input.length <= 3
+            }
+        },
+        {
+            type: 'input',
+            message: 'Enter a color (name or hexadecimal) for your logo',
+            name: 'color',
             validate: function (input) { 
                 return input.length > 0
             }
         },
         {
             type: 'list',
-            choices: ['MIT', 'Mozilla', 'Apache', 'None'],
-            message: 'Does your repository have a license?',
-            name: 'license'
+            choices: ['Square', 'Circle', 'Triangle'],
+            message: 'Choose a shape for your logo',
+            name: 'shape'
         },
     ])
     .then((response) => {
-        // console.log(response);
-        writeToFile('generatedREADME.md', response);
+        console.log(response);
     })
 }
 
 init();
+
+/*
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for text
+THEN I can enter up to three characters
+WHEN I am prompted for the text color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I am prompted for a shape
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+WHEN I am prompted for the shape's color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I have entered input for all the prompts
+THEN an SVG file is created named `logo.svg`
+AND the output text "Generated logo.svg" is printed in the command line
+WHEN I open the `logo.svg` file in a browser
+THEN I am shown a 300x200 pixel image that matches the criteria I entered
+*/
